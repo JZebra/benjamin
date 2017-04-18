38b9ceef5246360f25c36190fe3b97bf0a197766
John Shen <johnzhihaoshen@gmail.com>
2017-04-17 17:09:45 -0700

    registered virtue, virtueset, virtueentry to admin interface

--- benjamin/checklist/admin.py
+++ benjamin/checklist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from benjamin.checklist.models import Virtue, VirtueEntry, VirtueSet
 
 # Register your models here.
+admin.site.register(Virtue)
+admin.site.register(VirtueEntry)
+admin.site.register(VirtueSet)
