51f9ce21e06f503453c7a7c1235d2ef909222fb9
George Ashworth & John Shen <johnzhihaoshen@gmail.com>
2017-04-18 14:19:48 -0700

    removed redefinition of urlpatters in urls.py

--- benjamin/urls.py
+++ benjamin/urls.py
@@ -20,18 +20,12 @@ from rest_framework import routers
 
 from benjamin.checklist.views import UserViewSet
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-]
-
 # Routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
