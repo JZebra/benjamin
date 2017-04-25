bb573ac90c264707fab27be507ce40c6857a5ee2
George Ashworth & John Shen <johnzhihaoshen@gmail.com>
2017-04-24 18:09:55 -0700

    removed loginform from react app, auth will be handled serverside

--- web/src/App.js
+++ web/src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Button from './Button';
-import LoginForm from './LoginForm';
 import VirtueSetList from './VirtueSetList';
 
 class App extends Component {
   getVirtueSets() {
-    fetch('http://127.0.0.1:8000/virtue_sets/', {
+    fetch('http://:8000/api/virtue_sets/', {
       method: 'GET',
       headers: {
-        'Authorization': 'Token ' + localStorage.token,
+        'Authorization': 'Token ' + this.props.store.token,
         'Accept': 'application/json'
       }
     }).then((res) => {
       // TODO make list from items in json, pass it into VirtueSetList
       // return res.json();
+      console.log(res.json())
       return this.renderVirtueSetList(res.json());
     });
   }
@@ -35,8 +35,7 @@ class App extends Component {
         <p className="App-intro">
           Hello world!
         </p>
-        <Button onClick={()=>console.log('clicked')} value="click here"/>
-        <LoginForm store={this.props.store} afterLogin={this.getVirtueSets.bind(this)}/>
+        <Button onClick={this.getVirtueSets.bind(this)} value="click here"/>
       </div>
     );
   }
--- web/src/AppState.js
+++ web/src/AppState.js
@@ -22,3 +22,4 @@ export default class AppState {
         this.virtueSets = virtueSets;
     }
 }
+
