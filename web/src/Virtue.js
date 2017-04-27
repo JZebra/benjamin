597a38ee33c0aa49798ef0064f6234e8f0079307
George Ashworth & John Shen <johnzhihaoshen@gmail.com>
2017-04-26 17:26:12 -0700

    added buttons to Virtue component

--- web/src/Virtue.js
+++ web/src/Virtue.js
@@ -6,9 +6,18 @@ import { _fetch } from './utils';
 @observer
 export default class Virtue extends Component {
 
-  handleClick() {
+  handleSuccess() {
+    // TODO: pass the value in the click handler
+    this.handleClick(1)
+  }
+
+  handleFail() {
+    this.handleClick(0)
+  }
+
+  handleClick(value) {
     let payload = {
-      value: 1,
+      value: value,
       virtue_id: this.props.virtue.id,
     }
 
@@ -27,7 +36,10 @@ export default class Virtue extends Component {
       <div className="Virtue col-6" onClick={ this.handleClick.bind(this) }>
         <h1 className="Virtue-title">{ virtue.title }</h1>
         <p className="Virtue-quote">{ virtue.quote }</p>
+        <button className="btn btn-success" onClick={ this.handleSuccess.bind(this) }>Yes</button>
+        <button className="btn btn-danger" onClick={ this.handleFail.bind(this) }>No</button>
       </div>
     );
   }
 }
+
