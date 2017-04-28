6289e95ee273d0ea6a7d507c13b14ecaaa87c833
George Ashworth & John Shen <johnzhihaoshen@gmail.com>
2017-04-28 15:18:22 -0700

    cleaned up navbar stylings

--- web/src/Navbar.jsx
+++ web/src/Navbar.jsx
@@ -13,15 +13,27 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-default">
+      <nav className="navbar navbar-inverse bg-success navbar-toggleable-md">
+        <button className="navbar-toggler navbar-toggler-right" type="button"
+          data-toggle="collapse" data-target="#navbar-collapse">
+          <FaBars />
+        </button>
         <a href="#" className="navbar-brand">Benjamin</a>
-        <div className="dropdown">
-          <div className="dropdown-toggle" data-toggle="dropdown">
-            <FaBars />
-          </div>
-          <ul className="dropdown-menu">
-            <li onClick={ this.handleDayViewClick.bind(this) }>Dayview</li>
-            <li onClick={ this.handleChainViewClick.bind(this) }>Chainview</li>
+
+        <div className="collapse navbar-collapse" id="navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <button onClick={ this.handleDayViewClick.bind(this) }
+                type="button" className="nav-link btn btn-outline-success">
+                Day View
+              </button>
+            </li>
+            <li className="nav-item">
+              <button onClick={ this.handleChainViewClick.bind(this) }
+                type="button" className="nav-link btn btn-outline-success">
+                Chain View
+              </button>
+            </li>
           </ul>
         </div>
       </nav>
