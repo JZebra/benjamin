2f51ab3a3687c0a31640408f1833db70e109e626
George Ashworth & John Shen <johnzhihaoshen@gmail.com>
2017-05-08 13:33:06 -0700

    updated scrollDay to use check/cross icons instead of plaintext

--- web/src/ScrollDay.jsx
+++ web/src/ScrollDay.jsx
@@ -2,6 +2,8 @@
 
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
+import { FaCheck, FaTimesCircle } from 'react-icons/lib/fa';
+
 
 import './ScrollDay.scss';
 
@@ -9,13 +11,25 @@ import './ScrollDay.scss';
 @observer
 export default class ScrollDay extends Component {
 
+    renderValue(value) {
+        if (value === 1) {
+            return <FaCheck />;
+        }
+
+        if (value === 0) {
+            return <FaTimesCircle />;
+        }
+
+        return <p>?</p>;
+    }
+
 
     renderVirtueEntries() {
         // TODO: maintain order between Scrolldays
         return this.props.appStore.virtueEntryDateMap[this.props.date].map(ve => {
             return (
                 <div className="ScrollDay-VirtueEntry">
-                    { ve.value }
+                    { this.renderValue(ve.value) }
                 </div>
             )
         })
